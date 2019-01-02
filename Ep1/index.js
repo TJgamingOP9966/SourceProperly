2f02ea996190975b362bda06d349e482305f41bd
Stan Barkmeijer <stanbarkmeijer@hotmail.com>
2019-01-02 16:19:18 +0100

    Ep1

---
+++ Ep1/index.js
@@ -0,0 +1,26 @@
+const { prefix, token } = require("./config.json");
+const { Client } = require("discord.js");
+
+const bot = new Client({
+    disableEveryone: true
+});
+
+bot.on("ready", () => {
+    console.log(`${bot.user.username} is now online!`);
+    bot.user.setActivity("proper code", {
+        type: "WATCHING"
+    });
+});
+
+bot.on("message", async message => {
+    if (message.author.bot || message.channel.type == "dm") return;
+
+    let args = message.content.slice(prefix.length).trim().split(/ +/g);
+    let cmd = args.shift().toLowerCase();
+
+    if (cmd == `${prefix}hello`) { 
+        return message.channel.send("Hello!");
+    }
+});
+
+bot.login(token)
