c1d7a0f79a48af880021b3682b1ebbde76761010
icourt1 <icourt.business@gmail.com>
2019-02-17 13:01:39 -0600

    Roles are no longer case sensitive

--- Ep7/commands/removerole.js
+++ Ep7/commands/removerole.js
@@ -4,11 +4,11 @@ module.exports = {
     },
     run: async (bot, message, args) => {
         let member = message.mentions.members.first();
-        let role = message.guild.roles.find(r => r.name == args.slice(1).join(" "));
+        let role = message.guild.roles.find(r => r.name.toLowerCase() == args.slice(1).join(" ").toLowerCase());
 
         if (!message.member.hasPermission(["MANAGE_ROLES"])) return message.reply("You don't have the specified permissions to do this!").then(m => m.delete(5000));
         if (!member) return message.reply("You need to mention someone").then(m => m.delete(5000));
-        if (!role) return message.reply("You need to use a valid role name, case sensitive too.").then(m => m.delete(5000));
+        if (!role) return message.reply("You need to use a valid role name!").then(m => m.delete(5000));
         if (member.roles.find(role)) {
             member.removeRole(role);
             try {
